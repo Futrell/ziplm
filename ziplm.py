baae89cb677786af8f596bef1f09f2f8f1db19fd
Richard Futrell <canjo@Richards-MacBook-Pro-2.local>
2023-07-14 13:48:36 -0700

    remove some useless joins

--- ziplm.py
+++ ziplm.py
@@ -12,31 +12,30 @@ class ZipModel:
         self.compressor = compressor
         self.index = {v:i for i, v in enumerate(self.vocabulary)}
 
-    def logprobs(self, prefix=""):
+    def logprobs(self, prefix="", temperature=1):
         code_lengths = np.array([
             len(self.compressor.compress("".join([self.training, prefix, v]).encode()))
             for v in self.vocabulary
         ])
-        return scipy.special.log_softmax(-code_lengths*np.log(len(self.vocabulary)), -1)
-
-    def sequence_logprob(self, sequence):
-        prefix = []
+        return scipy.special.log_softmax(-code_lengths*np.log(len(self.vocabulary))*(1/temperature))
+                                         
+    def sequence_logprob(self, sequence, prefix="", temperature=1):
         score = 0.0
         for x in sequence:
-            scores = self.logprobs("".join(prefix))
+            scores = self.logprobs(prefix, temperature=temperature)
             score += scores[self.index[x]]
-            prefix.append(x)
+            prefix += x
         return score
 
-    def sample(self, prefix=""):
-        scores = self.logprobs(prefix)
+    def sample(self, prefix="", temperature=1):
+        scores = self.logprobs(prefix, temperature=temperature)
         i = np.random.choice(range(len(self.vocabulary)), p=np.exp(scores))
         return self.vocabulary[i]
 
-    def sample_sequence(self, prefix, maxlen=None):
-        sequence = [prefix]
+    def sample_sequence(self, maxlen, prefix="", temperature=1):
+        sequence = prefix
         for k in range(maxlen):
-            result = self.sample("".join(sequence))
+            result = self.sample(sequence, temperature=temperature)
             yield result
-            sequence.append(result)
+            sequence += result
 
